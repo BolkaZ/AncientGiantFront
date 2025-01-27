963d183539de87abc9d3a8df4e1845c89239dfb9
Kirill <pegeoner@mail.ru>
2025-01-27 23:04:38 +0300

    add tauri to project

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,21 @@ import { BreadCrumbs } from './components/bread-crumbs/BreadCrumbs'
 import { Header } from './components/header/Header'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import {Container} from 'react-bootstrap';
+import {useEffect} from 'react';
+import {invoke} from '@tauri-apps/api/core';
 
 
 function App() {
-
+  useEffect(()=>{
+    invoke('tauri', {cmd:'create'})
+      .then(() =>{console.log("Tauri launched")})
+      .catch(() =>{console.log("Tauri not launched")})
+    return () =>{
+      invoke('tauri', {cmd:'close'})
+        .then(() =>{console.log("Tauri launched")})
+        .catch(() =>{console.log("Tauri not launched")})
+    }
+  }, [])
   return (
     <>
       <Header/>
